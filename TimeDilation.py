ee80b3174f444ad26af54846d24b188021262eb7
Henry Dowding <henrydowding@gmail.com>
2014-11-17 17:10:53 +0000

    Added Comments

--- TimeDilation.py
+++ TimeDilation.py
@@ -1,6 +1,6 @@
 
-
 c = 8.9875518e+16
+#Speed of light squared
 t = int(input("Please enter the time interval for the observer (s):"))
 v = int(input("Enter the velocity of moving object relative to you(m/s):"))
 vf = v*v
