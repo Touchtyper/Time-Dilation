29bf98b1ed3294546540f5d0501e656680ed66c9
Henry Dowding <Touchtyper@users.noreply.github.com>
2017-05-16 11:48:09 +0100

    Added More Comments + Changed Variable Name

--- TimeDilation.py
+++ TimeDilation.py
@@ -1,12 +1,12 @@
 
 c = 8.9875518e+16
-#Speed of light squared
+# Speed of light squared
 t = int(input("Please enter the time interval for the observer (s):"))
 v = int(input("Enter the velocity of moving object relative to you(m/s):"))
 vf = v*v
 half = (1-vf/c)**(0.5)
-yes = t/half
+result = t/half
 
-print yes
+print result
 
 
